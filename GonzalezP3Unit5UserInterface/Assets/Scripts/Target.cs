eaef04fe932ffe53dae78bc2a1640bda49c68326
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2024-04-26 11:40:01 -0700

    Got score n can destroy
    I can destroy the boxs and gert a score

--- GonzalezP3Unit5UserInterface/Assets/Scripts/Target.cs
+++ GonzalezP3Unit5UserInterface/Assets/Scripts/Target.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManger gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -4;
     void Start()
     {
+       targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("GameManger").GetComponent<GameManger>();
+
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
         transform.position = RandomSpawnPos();
     }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        gameManager.UpdateScore(5);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
